ff6fa499a753437906ebe474b2469a20590d1c17
Karolina Kasperek <k.kasperek@stud.elka.pw.edu.pl>
2024-05-25 18:14:58 +0200

    Prototype of multiplication with shadow
    Signed-off-by: Karolina Kasperek <k.kasperek@stud.elka.pw.edu.pl>

--- main.c
+++ main.c
@@ -2,18 +2,40 @@
 #include "matrix_shd.h"
 
 int main(void) {
-    float data[] = {0, 4.4, 0, 0, 0, 7.8, 9.9, 0, 1.2};
-    unsigned int rows = 3;
-    unsigned int col = 3;
-    const matrix_t test_matrix = {rows, col, data};
+    float data_A[] = {0, 4.4, 0, 0, 0, 7.8, 9.9, 0, 1.2, 3, 3, 3};
+    unsigned int rows_A = 4;
+    unsigned int col_A = 3;
 
-    print_matrix(test_matrix);
-    shd_t *shd = (shd_t*) malloc(sizeof(shd_t *));
-    int coverage = matrix_getshd(&test_matrix, shd);
+    float data_B[]  = {3.3,5.5,6.6, 0, 0,0};
+
+    unsigned int rows_B = 3;
+    unsigned int col_B = 2;
+
+    const matrix_t test_matrix_A = {rows_A, col_A, data_A};
+    const matrix_t test_matrix_B = {rows_B, col_B, data_B};
+    matrix_t *output_matrix = (matrix_t*) malloc(sizeof(matrix_t));
+    printf("Test matrix A:\n");
+    print_matrix(test_matrix_A);
+    shd_t *shd_A = (shd_t*) malloc(sizeof(shd_t));
+    shd_t *shd_B = (shd_t*) malloc(sizeof(shd_t));
+
+    int coverageA = matrix_getshd(&test_matrix_A, shd_A);
+    int coverageB = matrix_getshd(&test_matrix_B, shd_B);
 
     /** Prints out list of lists **/
-    for (int i = 0; i <  test_matrix.rows; i++) {
-        shd_node *ptr = shd->shd_data[i];
+    printf("LIST A\n");
+    for (int i = 0; i <  test_matrix_A.rows; i++) {
+        shd_node *ptr = shd_A->shd_data[i];
+        printf("list %d: ", i);
+        while (ptr != NULL) {
+            printf("%d -> ", ptr->col);
+            ptr = ptr->next;
+        }
+        printf("\n");
+    }
+    printf("LIST B\n");
+    for (int i = 0; i <  test_matrix_B.rows; i++) {
+        shd_node *ptr = shd_B->shd_data[i];
         printf("list %d: ", i);
         while (ptr != NULL) {
             printf("%d -> ", ptr->col);
@@ -22,5 +44,10 @@ int main(void) {
         printf("\n");
     }
 
+    printf("====\n");
+    matrix_prodshd(&test_matrix_A, &test_matrix_B,  shd_A,  shd_B, output_matrix);
+
+    printf("Output matrix:\n");
+    print_matrix(*output_matrix);
     return 0;
 }
--- src/matrix_shd.c
+++ src/matrix_shd.c
@@ -13,7 +13,7 @@ int matrix_getshd(const matrix_t *A, shd_t *shd) {
     for (int i = 0; i < A->rows; i++) {
         shd_node *head = NULL;
         for (int j = 0; j < A->cols; j++) {
-            if (A->data[i * A->rows + j] > 0) {
+            if (A->data[i * A->cols + j] > 0) {
                 shd_node *node = (shd_node *) malloc(sizeof(shd_node));
                 node->col = j;
                 node->next = NULL;
@@ -22,6 +22,40 @@ int matrix_getshd(const matrix_t *A, shd_t *shd) {
                 } else
                     head->next = node;
                 head = node;
+            }
+        }
+    }
+    printf("\n");
+    return 0;
+}
+
+int matrix_prodshd(const matrix_t *A, const matrix_t *B, const shd_t *shdA, shd_t *shdB, matrix_t *C) {
+    /** checking if matrix dim are compatible **/
+    if (A -> cols != B -> rows)
+        return -1;
+    C->rows = A->rows;
+    C->cols = B->cols;
+    C->data = calloc(C->rows*C->cols, sizeof(float));
+    if (shdA == NULL || shdB == NULL)
+        // do naive multiplication
+        printf("Naive mult\n");
+    else {
+        // multiplication with shadow
+        for (int i=0; i < A-> rows; i++) {
+            shd_node *ptrA = shdA->shd_data[i];
+            printf("idx: %d\n", i);
+            while(ptrA != NULL){
+                shd_node *ptrB = shdB->shd_data[ptrA->col];
+                while(ptrB!=NULL){
+                    float val1=A->data[i*A->cols + ptrA->col];
+                    float val2 =B->data[ptrA->col*B->cols + ptrB->col];
+                    int idx_row = i;
+                    int idx_col = ptrB -> col;
+                    printf("c[%d][%d] += %.2f * %.2f\n", idx_row, idx_col, val1, val2);
+                    C->data[i*C->cols + ptrB -> col] +=  val1*val2 ;
+                    ptrB = ptrB -> next;
+                }
+                ptrA = ptrA->next;
             }
         }
     }
